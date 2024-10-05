b29bdaf2b11f15c1c50f2c0153a0a9f9402582c6
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2024-10-05 21:39:14 +0800

    Fixed Layout of landing Page

--- unipins/src/App.vue
+++ unipins/src/App.vue
@@ -1,84 +1,34 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="grid grid-rows-[auto_1fr] grid-cols-6 gap-6 w-screen h-screen p-6">
+    <!-- Header Bar -->
+    <header class="col-span-6 bg-sky-500 p-4 text-white rounded-[25px]">
+      <h1 class="text-xl font-bold">Main Header Bar</h1>
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <!-- Navbar -->
+    <nav class="col-span-1 bg-slate-900 p-4 rounded-[25px]">
+      <ul class="space-y-4">
+        <li>
+          <RouterLink to="/" class="text-white hover:text-gray-200">Home</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about" class="text-white hover:text-gray-200">About</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/profile" class="text-white hover:text-gray-200">Profile</RouterLink>
+        </li>
+      </ul>
+    </nav>
 
-  <RouterView />
+    <!-- Main Content -->
+    <main class="col-span-5 bg-slate-800 p-6 text-white rounded-[25px]">
+      <RouterView />
+    </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<!-- <style scoped></style> -->
--- unipins/src/views/HomeView.vue
+++ unipins/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="pin">
+    <h1>This is the pin page</h1>
+  </div>
 </template>
+
+<style></style>
